a46b3481bee4eae65e7dcfcc83aa2deb812f9ddc
korks2931 <korks2931@wrdsb.ca>
2020-10-13 14:03:59 +0000

    finished a7q2

--- Main.java
+++ Main.java
@@ -1,15 +1,34 @@
+import java.util.Scanner; 
 /**
- *
- * @author 
+ * This program will give the factors of the given number 
+ * @author Sevde
  */
 public class Main {
+  // A method to find the factors of a number 
+  public static void numberFactors (int numberUser){
+   // make a for loop to print out the factors of the number 
+    for ( int i = 1; i <= numberUser; i++){
+     int remainder = numberUser % i;
+     // make an if statemnt to find which numbers have remainder of 0
+     if (remainder == 0){
+       System.out.println(i);
+     }
+    }
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a Scanner for user input 
+    Scanner input= new Scanner(System.in);
+    // variable for the user input number 
+    int userNumber = input.nextInt();
+    // tell the user what number print out what factors 
+    System.out.println("factors(" + userNumber + ") would print out the numbers");
+    // do the method
+    numberFactors(userNumber) ;
     
   }
 }
